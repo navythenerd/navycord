15f7a1164602d6f0e6f9757fb19840580b89979d
Cedrik Kaufmann <cedrik@navythenerd.com>
2023-10-20 20:44:30 +0200

    update discord bot

--- bot/discord/handler.go
+++ bot/discord/handler.go
@@ -4,6 +4,6 @@ type Handler interface{}
 
 func (s *Service) registerHandler() {
 	s.Session().AddHandlerOnce(s.discordReady())
-	s.Session().AddHandlerOnce(s.discordRulesHandler())
 	s.Session().AddHandler(s.applicationCommandHandler())
+	s.Session().AddHandler(agreeRulesReactionHandler(s.config.RulesMessageId, s.config.AgreeRulesEmoteReaction, s.config.VerifiedRoleId))
 }
--- bot/discord/rules.go
+++ bot/discord/rules.go
@@ -2,10 +2,8 @@ package discord
 
 import (
 	"log"
-	"os"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/navythenerd/nerdguardian/bot/storage"
 )
 
 func agreeRulesReactionHandler(rulesMessageId string, agreeRulesEmoteReaction string, verfiedRoleId string) interface{} {
@@ -19,61 +17,5 @@ func agreeRulesReactionHandler(rulesMessageId string, agreeRulesEmoteReaction st
 				log.Print(err)
 			}
 		}
-	}
-}
-
-func (discordService *Service) discordRulesHandler() Handler {
-	return func(discordSession *discordgo.Session, r *discordgo.Ready) {
-		query := discordService.storage.DB().Model(&storage.Message{})
-		query = query.Where("annotation = ?", "rules").Limit(1)
-		var rulesMessages []storage.Message
-		query.Find(&rulesMessages)
-
-		mdRules, err := os.ReadFile(discordService.config.Rules)
-
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		var m *discordgo.Message
-
-		if len(rulesMessages) != 1 {
-			m, err = discordSession.ChannelMessageSend(discordService.config.RulesChannelId, string(mdRules))
-
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-		} else {
-			msg := rulesMessages[0]
-			// try to edit existing message
-			m, err = discordSession.ChannelMessageEdit(discordService.config.RulesChannelId, msg.ID, string(mdRules))
-
-			if err != nil {
-				discordService.storage.DB().Delete(msg)
-				log.Println(err)
-
-				// message doesn't exist anymore try to create a new rules message
-				m, err = discordSession.ChannelMessageSend(discordService.config.RulesChannelId, string(mdRules))
-
-				if err != nil {
-					log.Println(err)
-					return
-				}
-			}
-		}
-
-		ruleMessage := storage.Message{
-			ID:         m.ID,
-			User:       m.Author.ID,
-			Content:    m.Content,
-			Annotation: "rules",
-		}
-
-		discordSession.AddHandler(agreeRulesReactionHandler(m.ID, discordService.config.AgreeRulesEmoteReaction, discordService.config.VerifiedRoleId))
-
-		discordService.storage.DB().Save(&ruleMessage)
 	}
 }
