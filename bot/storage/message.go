2c585d2b1d3a4a44563214c24cee10ce9d20e5fb
Cedrik Kaufmann <cedrik@navythenerd.com>
2023-10-20 20:44:39 +0200

    update storage service

--- bot/storage/message.go
+++ bot/storage/message.go
@@ -3,10 +3,9 @@ package storage
 import "time"
 
 type Message struct {
-	ID         string `gorm:"primaryKey"`
-	User       string
-	Content    string
-	Annotation string
-	CreatedAt  time.Time `gorm:"column:created_at"`
-	UpdatedAt  time.Time `gorm:"column:updated_at"`
+	ID        string `gorm:"primaryKey"`
+	User      string
+	Content   string
+	CreatedAt time.Time `gorm:"column:created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at"`
 }
